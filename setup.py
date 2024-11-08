d7667792e72cc76f0e1ce4178cd9202a4d05c319
wlblount <bblount47@yahoo.com>
2024-11-08 15:04:45 -0500

    Initial commit with project files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fmp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'certifi',
+        'requests',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'tqdm',
+        'scikit-learn',
+        'python-dotenv'
+    ],
+    author='BillBlount',
+    author_email='',
+    description='A package for accessing Financial Modeling Prep API',
+    url='https://github.com/wlblount/fmp',
+)
